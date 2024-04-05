f0432188a0a72e15db933ea024d3faabdeb540a4
jburnoo <6pizz@naver.com>
2024-04-05 16:57:35 +0900

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "seoul_go"
+include(":app")
+ 
