09846c305d8ce51926bb7a5085ce094b9339a110
jburnoo <6pizz@naver.com>
2024-04-16 18:29:11 +0900

    Feat: #2 Remove overscroll effect

--- app/src/main/java/com/jbrunoo/seoul_go/presentation/ui/theme/Theme.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/presentation/ui/theme/Theme.kt
@@ -2,6 +2,8 @@ package com.jbrunoo.seoul_go.presentation.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -9,6 +11,7 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
@@ -80,6 +83,7 @@ private val DarkColorScheme = darkColorScheme(
     scrim = md_theme_dark_scrim,
 )
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Seoul_goTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -99,6 +103,10 @@ fun Seoul_goTheme(
     MaterialTheme(
       colorScheme = colorScheme,
       typography = Typography,
-      content = content
-    )
+    ) {
+        CompositionLocalProvider(
+            LocalOverscrollConfiguration provides null,
+            content = content
+        )
+    }
 }
