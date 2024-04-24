acbd3b66c1fff5b0806be9bd0907fd0c67267ffb
jburnoo <6pizz@naver.com>
2024-04-24 23:49:47 +0900

    feat:

--- app/src/main/java/com/jbrunoo/seoul_go/presentation/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/presentation/feature/home/HomeViewModel.kt
@@ -11,15 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val fetchEventUseCase: FetchEventUseCase): ViewModel() {
-    private var _events = MutableStateFlow<List<Event>>(emptyList())
-    val events = _events.asStateFlow()
+class HomeViewModel @Inject constructor(): ViewModel() {
 
-    init {
-        viewModelScope.launch {
-            fetchEventUseCase().collect {
-                _events.value = it
-            }
-        }
-    }
+
 }
