792f9aef15920922827b8b6a983fa18d47c75716
jburnoo <6pizz@naver.com>
2024-04-24 23:48:00 +0900

    feat: #12 Add domain event model

--- app/src/main/java/com/jbrunoo/seoul_go/domain/model/Event.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/domain/model/Event.kt
@@ -2,8 +2,26 @@ package com.jbrunoo.seoul_go.domain.model
 
 
 data class Event(
-    val id: Int,
-    val name: String,
-    val image: String,
-    val isFavorite: Boolean,
+    val CODENAME: String,
+    val DATE: String,
+    val END_DATE: String,
+    val ETC_DESC: String,
+    val GUNAME: String,
+    val IS_FREE: String,
+    val LAT: String?,
+    val LOT: String?,
+    val MAIN_IMG: String,
+    val ORG_LINK: String,
+    val ORG_NAME: String,
+    val PLACE: String,
+    val PLAYER: String,
+    val PROGRAM: String,
+    val RGSTDATE: String,
+    val STRTDATE: String,
+    val THEMECODE: String,
+    val TICKET: String,
+    val TITLE: String,
+    val USE_FEE: String,
+    val USE_TRGT: String,
+    val IS_FAVORITE: Boolean
 )
