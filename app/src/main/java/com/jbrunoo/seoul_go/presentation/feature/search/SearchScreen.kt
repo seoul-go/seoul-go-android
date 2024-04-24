928358d777021e63436292b4561285fbb0137911
jburnoo <6pizz@naver.com>
2024-04-24 23:52:35 +0900

    feat: #6 Modify search screen and component

--- app/src/main/java/com/jbrunoo/seoul_go/presentation/feature/search/SearchScreen.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/presentation/feature/search/SearchScreen.kt
@@ -1,15 +1,27 @@
 package com.jbrunoo.seoul_go.presentation.feature.search
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
+import com.jbrunoo.seoul_go.domain.model.SearchHistory
 import com.jbrunoo.seoul_go.presentation.feature.search.ui.SearchTopBar
 import com.jbrunoo.seoul_go.presentation.ui.theme.Seoul_goTheme
 
@@ -20,18 +32,42 @@ fun SearchScreen(navHostController: NavHostController) {
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
-
+        SearchTopBar(value = searchWord,
+            navigateUp = { navHostController.navigateUp() },
+            onValueChange = { searchWord = it },
+            onDeleteWord = { searchWord = "" }
+        )
+        SearchHistoryBox()
     }
-    SearchTopBar(value = searchWord,
-        navigateUp = { navHostController.navigateUp() },
-        onValueChange = { searchWord = it },
-        onDeleteWord = { searchWord = "" }
-    )
 }
 
 @Composable
-fun SearchHistoryBox() {
+fun SearchHistoryBox(
+    searchHistoryList: List<SearchHistory> = emptyList()
+) {
+    Box {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Absolute.SpaceBetween
+            ) {
+                Text(text = "최근 검색어", fontWeight = FontWeight.ExtraBold)
+                Text(text = "전체 삭제", fontWeight = FontWeight.Light, modifier = Modifier.clickable {  })
+            }
+            if(searchHistoryList.isEmpty()) {
+                Text(text = "최근 검색어가 없습니다.")
+            } else {
+                LazyRow {
+                    items(searchHistoryList) { item ->
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = item.searchWord)
+                        }
+                    }
+                }
+            }
 
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/jbrunoo/seoul_go/presentation/feature/search/ui/SearchTopBar.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/presentation/feature/search/ui/SearchTopBar.kt
@@ -2,29 +2,30 @@ package com.jbrunoo.seoul_go.presentation.feature.search.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.jbrunoo.seoul_go.R
-import com.jbrunoo.seoul_go.presentation.components.TopBar
 import com.jbrunoo.seoul_go.presentation.ui.theme.LocalAppDimens
 
 @Composable
@@ -34,17 +35,27 @@ fun SearchTopBar(
     navigateUp: () -> Unit,
     onDeleteWord: (String) -> Unit
 ) {
-    TopBar(
-        showNavIcon = true,
-        navigateUp = navigateUp
-    ) {
-        CustomTextField(
-            value = value,
-            onValueChange = onValueChange,
-            onDeleteWord = { onDeleteWord(it) })
-    }
+    TopAppBar(
+        title = {
+            CustomTextField(
+                value = value,
+                onValueChange = onValueChange,
+                onDeleteWord = { onDeleteWord(it) })
+        },
+        navigationIcon = {
+            IconButton(onClick = navigateUp) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.arrow_left_light),
+                    contentDescription = "back arrow"
+                )
+            }
+        },
+        backgroundColor = MaterialTheme.colorScheme.background,
+        elevation = 0.dp
+    )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun CustomTextField(
     value: String,
@@ -52,10 +63,11 @@ private fun CustomTextField(
     onDeleteWord: (String) -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+
     BasicTextField(modifier = modifier
-        .fillMaxWidth()
         .height(40.dp)
-        .offset(x = (-16).dp)
+        .fillMaxSize()
         .clip(RoundedCornerShape(LocalAppDimens.current.cornerRadius))
         .background(
             MaterialTheme.colorScheme.background
@@ -69,22 +81,33 @@ private fun CustomTextField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
+        interactionSource = interactionSource,
         cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
         decorationBox = { innerTextField ->
-            Row(
-                modifier = modifier.padding(start = LocalAppDimens.current.innerPadding),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(imageVector = ImageVector.vectorResource(R.drawable.search_light), contentDescription = "search")
-                Box(Modifier.weight(1f)) {
-                    if (value.isEmpty()) Text("행사명을 입력하세요")
-                    innerTextField()
+            TextFieldDefaults.DecorationBox(
+                value = value,
+                innerTextField = innerTextField,
+                enabled = true,
+                singleLine = true,
+                visualTransformation = VisualTransformation.None,
+                interactionSource = interactionSource,
+                placeholder = { Text("행사명을 입력하세요") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.search_light),
+                        contentDescription = "search"
+                    )
+                },
+                trailingIcon = {
+                    IconButton(onClick = { onDeleteWord(value) }) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.close_ring),
+                            contentDescription = "clear all"
+                        )
+                    }
                 }
-                IconButton(onClick = { onDeleteWord(value) }) {
-                    Icon(imageVector = ImageVector.vectorResource(R.drawable.close_ring), contentDescription = "close all text")
 
-                }
-            }
+            )
         }
     )
 }
