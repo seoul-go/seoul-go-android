8e7b5575a201a86c967d445752736d4507990213
jburnoo <6pizz@naver.com>
2024-05-02 21:02:58 +0900

    feat: #14 최근 업데이트된 행사 가져오는 로직 추가
    공통 에러 핸들링 부분 inline, generic 활용하여 추출

--- app/src/main/java/com/jbrunoo/seoul_go/data/repository/EventRepositoryImpl.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/data/repository/EventRepositoryImpl.kt
@@ -1,16 +1,14 @@
 package com.jbrunoo.seoul_go.data.repository
 
-import android.util.Log
 import com.jbrunoo.seoul_go.common.Resource
 import com.jbrunoo.seoul_go.data.dataSource.remote.EventRemoteDataSource
+import com.jbrunoo.seoul_go.data.dataSource.remote.dto.RESULT
 import com.jbrunoo.seoul_go.data.dataSource.remote.dto.toEvent
+import com.jbrunoo.seoul_go.data.dataSource.remote.dto.toRecentEventImage
 import com.jbrunoo.seoul_go.domain.model.Event
+import com.jbrunoo.seoul_go.domain.model.RecentEventImage
 import com.jbrunoo.seoul_go.domain.repository.EventRepository
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import retrofit2.HttpException
 import timber.log.Timber
@@ -19,42 +17,62 @@ import javax.inject.Inject
 
 class EventRepositoryImpl @Inject constructor(private val eventRemoteDataSource: EventRemoteDataSource) :
     EventRepository {
-    override fun fetchEventsByCodeFlow(codeName: String): Flow<Resource<List<Event>>> {
-        return eventRemoteDataSource.fetchEventsByCodeNameFlow(codeName = codeName).map { eventResponse ->
-            val result = eventResponse.culturalEventInfo.result
-            val row = eventResponse.culturalEventInfo.row
+    override fun fetchEventsByCodeFlow(
+        codeName: String,
+        title: String
+    ): Flow<Resource<List<Event>>> {
+        return eventRemoteDataSource.fetchEventsByCodeNameFlow(codeName = codeName, title = title)
+            .map { eventResponse ->
+                val result = eventResponse.culturalEventInfo.result
+                val row = eventResponse.culturalEventInfo.row
 
-            try {
-                when (result.code) {
-                    "INFO-000" -> {
-                        Timber.d("SUCCESS")
-                        val events = row.map { it.toEvent() }
-                        Resource.Success(events)
-                    }
-                    "INFO-200" -> {
-                        Timber.d("INFO-200")
-                        Resource.Error(message = result.message)
-                    }
-                    "ERROR-500", "ERROR-600", "ERROR-601" -> {
-                        Timber.d("SERVER-ERROR")
-                        Resource.Error(message = "서버 데이터 점검 중 입니다.")
-                    }
-                    else -> {
-                        Timber.d("OTHER ERRORS")
-                        Resource.Error(message = "데이터 요청에 실패하였습니다")
-                    }
+                if(result.code == "INFO-000") {
+                    Timber.d("SUCCESS")
+                    val events = row.map { it.toEvent() }
+                    Resource.Success(events)
+                } else {
+                    handleResult(result = result)
                 }
-            } catch (e: HttpException) {
-                Timber.d("HTTP EXCEPTION")
-                Resource.Error(e.localizedMessage ?: "HTTP EXCEPTION")
-            } catch (e: IOException) {
-                Timber.d("IO EXCEPTION")
-                Resource.Error("IO EXCEPTION")
             }
-        }
     }
 
-    override fun searchEventsFlow(codeName: String, title: String): Flow<Resource<List<Event>>> {
-        TODO("Not yet implemented")
+    override fun fetchRecentEventsFlow(endIndex: Int): Flow<Resource<List<RecentEventImage>>> {
+        return eventRemoteDataSource.fetchRecentEventsFlow(endIndex = endIndex).map { eventResponse ->
+                val result = eventResponse.culturalEventInfo.result
+                val row = eventResponse.culturalEventInfo.row
+
+                if(result.code == "INFO-000") {
+                    Timber.d("SUCCESS")
+                    val eventImages = row.map { it.toRecentEventImage() }
+                    Resource.Success(eventImages)
+                } else {
+                    handleResult(result = result)
+                }
+            }
+    }
+
+    private inline fun <reified T> handleResult(result: RESULT): Resource<T> {
+        return try {
+            when (result.code) {
+                "INFO-200" -> {
+                    Timber.d("INFO-200")
+                    Resource.Error(message = result.message)
+                }
+                "ERROR-500", "ERROR-600", "ERROR-601" -> {
+                    Timber.d("SERVER-ERROR")
+                    Resource.Error(message = "서버 데이터 점검 중 입니다.")
+                }
+                else -> {
+                    Timber.d("OTHER ERRORS")
+                    Resource.Error(message = "데이터 요청에 실패하였습니다")
+                }
+            }
+        } catch (e: HttpException) {
+            Timber.d("HTTP EXCEPTION")
+            Resource.Error(e.localizedMessage ?: "HTTP EXCEPTION")
+        } catch (e: IOException) {
+            Timber.d("IO EXCEPTION")
+            Resource.Error("IO EXCEPTION")
+        }
     }
 }
