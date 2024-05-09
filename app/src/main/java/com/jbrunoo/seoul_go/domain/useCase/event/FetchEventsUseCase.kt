f11a362781e5f5f0cdd6898ff5ffbf64fc037dbc
jburnoo <6pizz@naver.com>
2024-05-09 20:20:34 +0900

    Style: Add default parameter

--- app/src/main/java/com/jbrunoo/seoul_go/domain/useCase/event/FetchEventsUseCase.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/domain/useCase/event/FetchEventsUseCase.kt
@@ -8,5 +8,5 @@ import javax.inject.Inject
 
 // useCase는 interface 등이 아니므로 @Module을 만들 필요 없음
 class FetchEventsUseCase @Inject constructor(private val eventRepository: EventRepository) {
-    operator fun invoke(codeName: String, title: String = " "): Flow<Resource<List<Event>>> = eventRepository.fetchEventsByCodeFlow(codeName = codeName, title = title)
+    operator fun invoke(codeName: String = " ", title: String = " "): Flow<Resource<List<Event>>> = eventRepository.fetchEventsByCodeFlow(codeName = codeName, title = title)
 }
