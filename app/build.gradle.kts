b1439f23b93e13bb92f952b02f6a737eed941502
jburnoo <6pizz@naver.com>
2024-05-09 20:10:45 +0900

    feat: Modify minSdk 24 -> 26, Add browser lib
    LocalDate 라이브러리 java 8, sdk 26 이상 지원
    행사 홈페이지 url 연결을 위한 browser 라이브러리 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jbrunoo.seoul_go"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -73,6 +73,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.browser)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
