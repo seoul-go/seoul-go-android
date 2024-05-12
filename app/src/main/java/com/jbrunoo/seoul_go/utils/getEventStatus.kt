eae8b4d5601837ec1037a01aa5528385a0536bb4
jburnoo <6pizz@naver.com>
2024-05-12 23:56:32 +0900

    feat: Add func 'getEventStatus'
    행사 날짜와 오늘 날짜를 비교하여 공연 상태를 반환

--- app/src/main/java/com/jbrunoo/seoul_go/utils/getEventStatus.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/utils/getEventStatus.kt
@@ -5,8 +5,8 @@ import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-fun getEventStatus(strtDate: String, endDate: String): String {
-    if (strtDate.isBlank() || endDate.isBlank()) return "정보없음"
+fun getEventStatus(strtDate: String, endDate: String): EventStatus {
+    if (strtDate.isBlank() || endDate.isBlank()) return EventStatus.UNKNOWN
 
     val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S")
     // 시작, 마감 시간이 둘 다 00:00:00.0 이기 때문에 날짜 부분만 사용
@@ -16,8 +16,15 @@ fun getEventStatus(strtDate: String, endDate: String): String {
 
     Timber.d("현재 시간 : $current")
     return when {
-        current.isBefore(strt) -> "공연예정"
-        current.isAfter(end) -> "공연마감"
-        else -> "공연중"
+        current.isBefore(strt) -> EventStatus.UPCOMING
+        current.isAfter(end) -> EventStatus.CLOSED
+        else -> EventStatus.ONGOING
     }
+}
+
+enum class EventStatus(val status: String) {
+    ONGOING("공연 중"),
+    UPCOMING("공연 예정"),
+    CLOSED("공연 마감"),
+    UNKNOWN("정보 없음")
 }
