0200ae2399555f6e699a47e27b9f7eaa3ead123e
jburnoo <6pizz@naver.com>
2024-06-17 23:57:38 +0900

    feat: #9 kakaoSdk 추가

--- app/src/main/java/com/jbrunoo/seoul_go/SeoulGoApplication.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/SeoulGoApplication.kt
@@ -1,16 +1,18 @@
 package com.jbrunoo.seoul_go
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
 @HiltAndroidApp
-class SeoulGoApplication: Application() {
+class SeoulGoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        if(BuildConfig.DEBUG){
+        if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
     }
 }
