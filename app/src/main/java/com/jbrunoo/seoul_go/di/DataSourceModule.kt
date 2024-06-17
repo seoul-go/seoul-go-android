10c6c272fcaece39e68272139ba7d58d69f8bf16
jburnoo <6pizz@naver.com>
2024-06-17 23:56:29 +0900

    feat: user, auth 로컬 데이터 소스 추가

--- app/src/main/java/com/jbrunoo/seoul_go/di/DataSourceModule.kt
+++ app/src/main/java/com/jbrunoo/seoul_go/di/DataSourceModule.kt
@@ -1,9 +1,13 @@
 package com.jbrunoo.seoul_go.di
 
+import com.jbrunoo.seoul_go.data.dataSource.local.AuthLocalDataSource
+import com.jbrunoo.seoul_go.data.dataSource.local.AuthLocalDataSourceImpl
 import com.jbrunoo.seoul_go.data.dataSource.local.room.event.EventLocalDataSource
 import com.jbrunoo.seoul_go.data.dataSource.local.room.event.EventLocalDataSourceImpl
 import com.jbrunoo.seoul_go.data.dataSource.local.room.search.SearchLocalDataSource
 import com.jbrunoo.seoul_go.data.dataSource.local.room.search.SearchLocalDataSourceImpl
+import com.jbrunoo.seoul_go.data.dataSource.local.room.user.UserLocalDataSource
+import com.jbrunoo.seoul_go.data.dataSource.local.room.user.UserLocalDataSourceImpl
 import com.jbrunoo.seoul_go.data.dataSource.remote.EventRemoteDataSource
 import com.jbrunoo.seoul_go.data.dataSource.remote.EventRemoteDataSourceImpl
 import dagger.Binds
@@ -15,6 +19,14 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class DataSourceModule {
+
+    @Binds
+    @Singleton
+    abstract fun provideAuthLocalDataSource(authLocalDataSourceImpl: AuthLocalDataSourceImpl): AuthLocalDataSource
+
+    @Binds
+    @Singleton
+    abstract fun provideUserLocalDataSource(userLocalDataSourceImpl: UserLocalDataSourceImpl): UserLocalDataSource
 
     @Binds
     @Singleton
